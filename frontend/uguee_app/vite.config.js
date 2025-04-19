f62a07f8a24a6b49dc09ec016e4babd6d3f757ae
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-04-19 17:28:44 -0500

    pagina inicial

--- frontend/uguee_app/vite.config.js
+++ frontend/uguee_app/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
-    open: true
+    port: 5173
   }
 })
+
+
+
 
